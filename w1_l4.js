b0f1bba226b975e34b872ad6c6341198e7ad7252
EricSchles <ericschles@gmail.com>
2014-12-14 05:59:00 -0800

    level 4 done

---
+++ w1_l4.js
@@ -0,0 +1,10 @@
+// Grab the gems and go to the exit.
+
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.moveDown();
+this.moveUp();
+this.moveRight();
