d82e3a59107e2c5d9c9f707e4ce36241891a7c90
EricSchles <ericschles@gmail.com>
2014-12-14 06:34:40 -0800

    "w1_l15.js added"

---
+++ w1_l15.js
@@ -0,0 +1,8 @@
+// Build 3 fences to keep the ogres at bay!
+
+this.moveDown();
+this.buildXY("fence", 36, 34);
+this.buildXY("fence", 36, 31);
+this.buildXY("fence", 36, 26);
+
+loop{this.moveRight();}
