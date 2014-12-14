2f809a1be847ed3f1786c1cb78fbaf19e5de0565
EricSchles <ericschles@gmail.com>
2014-12-14 06:15:29 -0800

    level 10

---
+++ w1_l10.js
@@ -0,0 +1,11 @@
+// You can use a variable like a nametag.
+
+var enemy1 = "Kratt";
+var enemy2 = "Gert";
+var enemy3 = "Ursa";
+
+this.attack(enemy1);
+this.attack(enemy1);
+
+this.attack(enemy2);
+this.attack(enemy3);
