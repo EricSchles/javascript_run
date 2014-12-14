966d307b0a78edb0428d2c9afc641ad9957e1fce
EricSchles <ericschles@gmail.com>
2014-12-14 06:07:12 -0800

    level 7 done

---
+++ w1_l7.js
@@ -0,0 +1,9 @@
+// Loops are a better way of doing repetitive things.
+
+loop {
+    // Add commands in here to repeat.
+    this.moveRight();
+    this.moveRight();
+    this.moveUp();
+    this.moveUp();
+}
