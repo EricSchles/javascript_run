3714d7a7c141920a909f5838fefaf42fdf35f0ca
EricSchles <ericschles@gmail.com>
2014-12-14 06:17:08 -0800

    level 11 done

---
+++ w1_l11.js
@@ -0,0 +1,14 @@
+// Your hero doesn't know these enemy's names!
+// The glasses give you the findNearestEnemy ability.
+
+var enemy1 = this.findNearestEnemy();
+this.attack(enemy1);
+this.attack(enemy1);
+
+var enemy2 = this.findNearestEnemy();
+this.attack(enemy2);
+this.attack(enemy2);
+
+var enemy3 = this.findNearestEnemy();
+this.attack(enemy3);
+this.attack(enemy3);
