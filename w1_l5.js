68ce3c5faab3d0fdd6099065f3a75651d43a41cf
EricSchles <ericschles@gmail.com>
2014-12-14 06:01:23 -0800

    level 5 done

---
+++ w1_l5.js
@@ -0,0 +1,11 @@
+// Defend against Brak and Treg!
+// You must attack small ogres twice.
+
+this.moveRight();
+this.attack("Brak");
+this.attack("Brak");
+this.moveRight();
+this.moveRight();
+this.attack("Treg");
+this.attack("Treg");
+this.moveRight();
