5f54f34ad5ab733ae5bf31cb9109a935f87e0f70
EricSchles <ericschles@gmail.com>
2014-12-14 06:39:04 -0800

    "w2_l2.js added"

---
+++ w2_l2.js
@@ -0,0 +1,8 @@
+// Go to the end of the path and build a fence there.
+// Use your moveXY(x, y) function.
+
+this.moveXY(36, 60);
+this.moveXY(37, 13);
+this.buildXY("fire-trap", 71, 25);
+this.moveXY(37, 13);
+this.moveXY(73,61);
