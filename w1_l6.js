cfa085a8b2507f8f7811e055cb801c10cc1021a7
EricSchles <ericschles@gmail.com>
2014-12-14 06:04:10 -0800

    level 6 done

---
+++ w1_l6.js
@@ -0,0 +1,9 @@
+// Defeat the ogres.
+// Remember that they each take two hits.
+
+this.attack("Rig");
+this.attack("Rig");
+this.attack("Ack");
+this.attack("Ack");
+this.attack("Gurt");
+this.attack("Gurt");
